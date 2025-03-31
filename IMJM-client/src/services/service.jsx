1dcd956235bac8cfaa1b7b1ed465ab829fabee63
SeungMin2055 <seungmin2055@gmail.com>
2025-03-31 09:58:10 +0900

    [ADD] : react 전체적인 클라이언트 페이지 화면 설정, header, footer 초안 만듬

---
+++ IMJM-client/src/services/service.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div>
+            <h1>도메인별 api서비스, 데이터 변환 및 처리 로직, 외부 서비스 연동</h1>
+        </div>
+    );
+};
+
+export default Service;
