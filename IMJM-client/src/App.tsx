8d52d98d71284db15b7c5e8d940e89d87fe53d6d
shindongeok <ehrflqakstp@gmail.com>
2025-04-11 11:12:18 +0900

    임시저장

---
+++ IMJM-client/src/App.tsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home/Home';
+import SalonDetail from "./pages/HairSalon/SalonDetail";
+import HairSalon from "./pages/HairSalon/HairSalon";
+import Login from './pages/User/Login';
+import UserDetailRegister from './pages/User/UserDetailRegister';
+import UserTypeSelect from './pages/User/UserTypeSelect';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="app-container">
+                <Header />
+                <main className="content-area">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/hairSalon" element={<HairSalon />} />
+                        <Route path="/archive" element={<div>Archive Page (준비 중)</div>} />
+                        <Route path="/community" element={<div>Community Page (준비 중)</div>} />
+                        <Route path="/myPage" element={<div>My Page (준비 중)</div>} />
+                        <Route path="/salon/:id" element={<SalonDetail />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/user/login" element={<Login />} />
+                        <Route path="/user/register" element={<UserTypeSelect />} />
+                        <Route path="/user/register/step1" element={<UserDetailRegister />} />
+                        <Route path="/hairSalon/stylists/:salonId" element={<Stylists />} />
+                        <Route path="/hairSalon/reservation/:stylistId" element={<StylistSchedule />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
