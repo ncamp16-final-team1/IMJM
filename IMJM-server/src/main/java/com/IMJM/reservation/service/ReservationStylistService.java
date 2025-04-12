dcccf49221adc6a7581f2092bfec642a4fe06607
shindongeok <ehrflqakstp@gmail.com>
2025-04-12 12:05:49 +0900

    [CHORE]스타일리스트 코드  수정

--- IMJM-server/src/main/java/com/IMJM/reservation/service/ReservationStylistService.java
+++ IMJM-server/src/main/java/com/IMJM/reservation/service/ReservationStylistService.java
@@ -1,7 +1,5 @@
 package com.IMJM.reservation.service;
 
-import com.IMJM.common.entity.AdminStylist;
-
 import com.IMJM.reservation.dto.ReservationStylistDto;
 import com.IMJM.reservation.dto.StylistAndSalonDetailsDto;
 import com.IMJM.reservation.repository.AdminStylistRepository;
@@ -18,31 +16,17 @@ import java.util.stream.Collectors;
 public class ReservationStylistService {
     private final AdminStylistRepository adminStylistRepository;
 
-    // 특정 살롱의 스타일리스트 목록 조회
     @Transactional(readOnly = true)
-    public List<ReservationStylistDto> getStylistsBySalon(String salonId) {    // 특정 살롱의 스타일리스트 목록 조회
-        return adminStylistRepository.findBySalonId(salonId).stream()// 살롱 ID로 스타일리스트 검색
-                .map(this::convertAdminStylistToDto)    // 엔티티를 DTO로 변환
-                .collect(Collectors.toList());  // 리스트로 수집
-    }
-
-    // 엔티티를 DTO로 변환하는 정적 메서드
-    public ReservationStylistDto convertAdminStylistToDto(AdminStylist adminStylist) {
-        return ReservationStylistDto.builder()
-                .adminStylist(adminStylist)
-                .build();
+    public List<ReservationStylistDto> getStylistsBySalon(String salonId) {
+        return adminStylistRepository.findBySalonId(salonId).stream()
+                .map(ReservationStylistDto::new)
+                .collect(Collectors.toList());
     }
 
     public StylistAndSalonDetailsDto getStylistAndSalonDetails(Long stylistId) {
         return adminStylistRepository.findByStylistId(stylistId)
-                .map(this::convertStylistAndSalonDetail)
+                .map(StylistAndSalonDetailsDto::new)
                 .orElseThrow(() -> new EntityNotFoundException("해당 스타일리스트가 없습니다."));
-    }
-
-    public StylistAndSalonDetailsDto convertStylistAndSalonDetail(AdminStylist adminStylist){
-        return StylistAndSalonDetailsDto.builder()
-                .adminStylist(adminStylist)
-                .build();
     }
 
 
