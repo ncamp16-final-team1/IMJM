e19241007cc3398918012bb1f610b3060cf33431
shindongeok <ehrflqakstp@gmail.com>
2025-04-16 11:14:55 +0900

    #33[FEAT]: 예약시간대 선택시 서비스메뉴조회 기능 구현완료

--- IMJM-server/src/main/java/com/IMJM/reservation/service/ReservationStylistService.java
+++ IMJM-server/src/main/java/com/IMJM/reservation/service/ReservationStylistService.java
@@ -1,14 +1,13 @@
 package com.IMJM.reservation.service;
 
 import com.IMJM.common.entity.AdminStylist;
-import com.IMJM.common.entity.AdminStylist;
 import com.IMJM.common.entity.ServiceMenu;
 import com.IMJM.reservation.dto.ReservationServiceMenuDto;
 import com.IMJM.reservation.dto.ReservationStylistDto;
 import com.IMJM.reservation.dto.StylistAndSalonDetailsDto;
 import com.IMJM.reservation.repository.AdminStylistRepository;
 import com.IMJM.reservation.repository.ReservationRepository;
-import com.IMJM.reservation.repository.ServiceMenuRepository;
+import com.IMJM.admin.repository.ServiceMenuRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
