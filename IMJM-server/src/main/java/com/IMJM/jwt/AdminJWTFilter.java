ef23ff393e0713478db7f89b9e7de0b1dab8fbbe
limsc <roko786@naver.com>
2025-04-16 19:40:55 +0900

    [BUG] 로그인 처리

--- IMJM-server/src/main/java/com/IMJM/jwt/AdminJWTFilter.java
+++ IMJM-server/src/main/java/com/IMJM/jwt/AdminJWTFilter.java
@@ -7,13 +7,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
+@Component
 public class AdminJWTFilter extends OncePerRequestFilter {
 
     private final JWTUtil jwtUtil;
