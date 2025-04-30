10e19a7fdd2bc6da2916c76e2ada3ab588b77355
hahyeong <hahyeong_0817@ewhain.net>
2025-04-30 18:30:27 +0900

    [CHORE]deleteChatRoomCompletely 삭제

--- IMJM-server/src/main/java/com/IMJM/chat/repository/ChatRoomRepository.java
+++ IMJM-server/src/main/java/com/IMJM/chat/repository/ChatRoomRepository.java
@@ -2,9 +2,6 @@ package com.IMJM.chat.repository;
 
 import com.IMJM.common.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,9 +15,4 @@ public interface ChatRoomRepository  extends JpaRepository<ChatRoom, Long> {
     List<ChatRoom> findByUserIdOrderByLastMessageTimeDesc(String userId);
 
     List<ChatRoom> findBySalonIdOrderByLastMessageTimeDesc(String salonId);
-
-    // 특정 채팅방 완전 삭제 메서드 추가
-    @Modifying
-    @Query("DELETE FROM ChatRoom cr WHERE cr.id = :chatRoomId")
-    void deleteChatRoomCompletely(@Param("chatRoomId") Long chatRoomId);
 }
