355f21f29043f996434c508d932e9a07e31b34d6
limsc <roko786@naver.com>
2025-05-07 17:29:19 +0900

    버그 수정

--- IMJM-client/src/config.ts
+++ IMJM-client/src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'https://api.imjm-hair.com';
+export const API_BASE_URL = 'http://localhost:8080';
--- IMJM-client/vite.config.js
+++ IMJM-client/vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://api.imjm-hair.com',
+        target: 'http://localhost:8080',
         changeOrigin: true
       },
       '/ws': { // WebSocket에 대한 프록시 설정 추가
-        target: 'https://api.imjm-hair.com',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         ws: true, // WebSocket 프록시 활성화
       }
